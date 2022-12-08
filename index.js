7b3f2424d5924e48ae38ccd1e5d70f3ef600aad5
angelblane <120124003+angelblane@users.noreply.github.com>
2022-12-08 12:34:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
 // ***************************************************************************
 
+response.end("HELLO WORLD");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
